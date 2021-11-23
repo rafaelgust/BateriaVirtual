ea9d90b60ffb2b78b06798644159ac215dbd1edd
rafaelgust <rafael.appdevs@gmail.com>
2021-11-22 23:57:38 -0300

    script main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-const playingClass = 'playing',
-crashRide = document.getElementById('crash-ride'),
+crashRide = document.getElementById('crash-ride');
 hitHatTop = document.getElementById('hihat-top');
+
+const playingClass = 'playing',
+animateCrashOrRide = () => {
+    crashRide.style.transform = 'rotate(0deg) scale(1.5)';
+},
+animateHitHatClosed = () => {
+    hitHatTop.style.top = '170px';
+},
+playSound = e => {
+    const keyCode = e.keyCode;
+    keyElement = document.querySelector(`div[data-key="${keyCode}"]`);
+    if(!keyElement){return;}
+
+    const audioElement = document.querySelector(`audio[data-key="${keyCode}"]`);
+    audioElement.currentTime = 0;
+}
